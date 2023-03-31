f7a28cf8d55655d9261cdda62423fc9c860a1158
Nathan Costa <nkyucode@gmail.com>
2023-03-31 18:56:52 -0300

    feat: Link to login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <>
-      <h1 className='text-2xl text-red-200'>Devflix test</h1>
-    </>
-  )
+    return (
+        <>
+            <h1 className="text-8xl text-red-200">Devflix test</h1>
+            <Link
+                className="text-6xl text-red-700 cursor-pointer hover:text-red-950 transition"
+                href="/auth"
+            >
+                click to Login page
+            </Link>
+        </>
+    );
 }
-
