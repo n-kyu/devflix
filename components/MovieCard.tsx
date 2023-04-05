c173b214f95630130164e80b2f53e9541f4139be
Nathan Costa <nkyucode@gmail.com>
2023-04-05 18:04:01 -0300

    style: fix movie vw

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -24,7 +24,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
         bg-zinc-800
         col-span
         relative
-        h-[12vw] 
+        h-[13vw] 
         "
         >
             <img
